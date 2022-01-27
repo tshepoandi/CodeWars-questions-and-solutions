a4a4fad8bb69538221e6599514caca5ac5e422c9
tshepoandi <85301167+tshepoandi@users.noreply.github.com>
2022-01-27 08:52:58 +0200

    your order please

---
+++ Your-Oder-Please.js
@@ -0,0 +1,25 @@
+/** @format */
+
+// Instructions
+
+// Your task is to sort a given string. Each word in the string will contain a single number. This number is the position the word should have in the result.
+
+// Note: Numbers can be from 1 to 9. So 1 will be the first word (not 0).
+
+// If the input string is empty, return an empty string. The words in the input String will only contain valid consecutive numbers.
+
+// Examples
+// "is2 Thi1s T4est 3a"  -->  "Thi1s is2 3a T4est"
+// "4of Fo1r pe6ople g3ood th5e the2"  -->  "Fo1r the2 g3ood 4of th5e pe6ople"
+// ""  -->  ""
+
+function order(words) {
+	let obj = {};
+	let numInWords = words.match(/\d/g);
+	for (let i = 0; i < words.length; i++) {
+		if (numInWords[i] !== undefined) {
+			obj[numInWords[i]] = words.split(' ')[i];
+		}
+	}
+	return Object.values(obj).join(' ');
+}
